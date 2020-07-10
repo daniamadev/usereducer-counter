b46c4634951c95d828d5ea1dd01b0875bbd46e2b
Daniela Aparecida Amadeu de Proença <amadev@amadev.localhost>
2020-07-10 20:02:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      }
+    case "decrement":
+      return {
+        count: state.count - 1
+      }
+    case "reset":
+      return initialState
+    default:
+      return initialState
+  }
+
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center my-6">
+      <h1 className="text-lg m-2">Count: {state.count}</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+        onClick={() => dispatch({ type: "increment" })}>
+        Increment
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+        onClick={() => dispatch({ type: "reset" })}>
+        Reset
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+        onClick={() => dispatch({ type: "decrement" })}>
+        Decrement
+          </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+//const UserContext = React.createContext()
 
 ReactDOM.render(
   <React.StrictMode>
+    {/*  <UserContext.Provider> */}
     <App />
+    {/*   </UserContext.Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
